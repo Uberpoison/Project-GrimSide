cd26d57a179d8578625c8f0e772f94e311a587f1
JordyVDE <jordy12vde@gmail.com>
2024-04-22 20:18:03 +0200

    Final Part

---
+++ Source/Grimside/GrimsideGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GrimsideGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRIMSIDE_API AGrimsideGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
